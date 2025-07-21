7c3551162b92708715fe9bf850a8bf0181694a1d
Fkhrayef <alkhrayefff@gmail.com>
2025-07-21 23:16:40 +0300

    Roles (USER, ADMIN), Admin Panel, Database Hooks

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,11 +7,22 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const headersList = await headers();
+
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: headersList,
   });
 
   if (!session) redirect("/auth/login");
+
+  const FULL_POST_ACCESS = await auth.api.userHasPermission({
+    headers: headersList,
+    body: {
+      permissions: {
+        posts: ["update", "delete"],
+      },
+    },
+  });
 
   return (
     <div className="px-8 py-16 container mx-auto max-w-screen-lg space-y-8">
@@ -23,7 +34,23 @@ export default async function Page() {
         </Button>
         <h1 className="text-3xl font-bold">Profile</h1>
 
-        <SignOutButton />
+        <div className="flex items-center gap-2">
+          {session.user.role === "ADMIN" && (
+            <Button size="sm" asChild>
+              <Link href="/admin/dashboard">Admin Dashboard</Link>
+            </Button>
+          )}
+
+          <SignOutButton />
+        </div>
+        <h2 className="text-2xl font-bold">Permissions</h2>
+
+        <div className="space-x-4">
+          <Button size="sm">MANAGE OWN POSTS</Button>
+          <Button size="sm" disabled={!FULL_POST_ACCESS.success}>
+            MANAGE ALL POSTS
+          </Button>
+        </div>
 
         <pre className="text-sm overflow-clip">
           {JSON.stringify(session, null, 2)}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { admin } from "better-auth/plugins";
 
 import { prisma } from "@/lib/prisma";
 import { hashPassword, verifyPassword } from "@/lib/argon2";
+import { UserRole } from "@/generated/prisma";
+import { ac, roles } from "@/lib/permissions";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -21,6 +24,18 @@ export const auth = betterAuth({
   // hooks: {
   // here we can add hooks to the auth process
   // }
+  // databaseHooks: {
+  // here we can add hooks to the database, for example we can check the user email and right before creating the user
+  // we can determine if his role should be an ADMIN or not.
+  // },
+  user: {
+    additionalFields: {
+      role: {
+        type: ["USER", "ADMIN"] as Array<UserRole>,
+        input: false,
+      },
+    },
+  },
   session: {
     expiresIn: 60 * 60 * 24 * 30, // 30 days
   },
@@ -29,7 +44,15 @@ export const auth = betterAuth({
       generateId: false,
     },
   },
-  plugins: [nextCookies()],
+  plugins: [
+    nextCookies(),
+    admin({
+      defaultRole: UserRole.USER,
+      adminRoles: [UserRole.ADMIN],
+      ac,
+      roles,
+    }),
+  ],
 });
 
 export type ErrorCode = keyof typeof auth.$ERROR_CODES | "UNKNOWN";
